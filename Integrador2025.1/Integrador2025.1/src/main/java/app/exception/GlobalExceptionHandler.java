5fccf922fb46c38682658d451114332bac68f126
XundaBoy <ARTHURROBERTBUCHMARQUES@GMAIL.COM>
2025-04-12 17:46:14 -0300

    Merge branch 'master' of https://github.com/XundaBoy/KDS.git

---
+++ Integrador2025.1/Integrador2025.1/src/main/java/app/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package app.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGeneralException(Exception ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Erro interno no servidor");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
